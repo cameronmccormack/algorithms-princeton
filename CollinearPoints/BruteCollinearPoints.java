9c163037bcfcc9f4d932e2551add4032335c9b7b
Cameron McCormack <cam.mac@live.co.uk>
2019-09-01 19:12:09 +0100

    initial commit 100/100

---
+++ CollinearPoints/BruteCollinearPoints.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+    private double err = 0.00000001;
+    private ArrayList<LineSegment> segs = new ArrayList<>();
+
+    public BruteCollinearPoints(Point[] points) {
+        // check if whole array is null
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        // check if any points are null
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        // check for duplicates
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+
+        // make copy of array so that sorting doesn't mutate original data
+        Point[] copy = new Point[points.length];
+        for (int i = 0; i < points.length; i++) {
+            copy[i] = points[i];
+        }
+
+        // sort array of points so that the line segment through any 4 points is always between the first and last point in the array
+        Arrays.sort(copy);
+
+        double slope1, slope2, slope3;
+        for (int i = 0; i < points.length - 3; i++) {
+            for (int j = i + 1; j < points.length - 2; j++) {
+                for (int k = j + 1; k < points.length - 1; k++) {
+                    for (int l = k + 1; l < points.length; l++) {
+                        slope1 = copy[i].slopeTo(copy[j]);
+                        slope2 = copy[i].slopeTo(copy[k]);
+                        slope3 = copy[i].slopeTo(copy[l]);
+                        if (Math.abs(slope1 - slope2) < err && Math.abs(slope1 - slope3) < err) {
+                            segs.add(new LineSegment(copy[i], copy[l]));
+                        } else if (slope1 == Double.POSITIVE_INFINITY && slope2 == Double.POSITIVE_INFINITY && slope3 == Double.POSITIVE_INFINITY) {
+                            segs.add(new LineSegment(copy[i], copy[l]));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public int numberOfSegments() {
+        return segs.size();
+    }
+
+    public LineSegment[] segments() {
+        return segs.toArray(new LineSegment[segs.size()]);
+    }
+}
---
+++ CollinearPoints/FastCollinearPoints.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FastCollinearPoints {
+    private double err = 0.00000001;
+    private ArrayList<LineSegment> segs = new ArrayList<>();
+
+    public FastCollinearPoints(Point[] points) {        
+        // check if whole array is null
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        // check if any points are null
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        // check for duplicates
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+
+        // create copy of points that can be sorted, then sort it
+        Point[] copy = new Point[points.length];
+        for (int i = 0; i < points.length; i++) {
+            copy[i] = points[i];
+        }
+        
+        for (int i = 0; i < copy.length - 3; i++) {
+            Arrays.sort(copy);
+
+            // Sort the points according to the slopes they makes with p.
+            // Check if any 3 (or more) adjacent points in the sorted order
+            // have equal slopes with respect to p. If so, these points,
+            // together with p, are collinear.
+
+            Arrays.sort(copy, copy[i].slopeOrder());
+
+            for (int p = 0, first = 1, last = 2; last < copy.length; last++) {
+                // find last collinear to p point
+                while (last < copy.length
+                        && Double.compare(copy[p].slopeTo(copy[first]), copy[p].slopeTo(copy[last])) == 0) {
+                    last++;
+                }
+                // if found at least 3 elements, make segment if it's unique
+                if (last - first >= 3 && copy[p].compareTo(copy[first]) < 0) {
+                    segs.add(new LineSegment(copy[p], copy[last - 1]));
+                }
+                // Try to find next
+                first = last;
+            }
+        }
+        // finds all line segments containing 4 or more points
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return segs.size();
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return segs.toArray(new LineSegment[segs.size()]);
+    }
+}
