a3c4db348bf2e839af8ebb812111806a860406c5
Cameron McCormack <cam.mac@live.co.uk>
2019-09-02 16:57:51 +0100

    updated to make less calls to stddev() and mean() by only calling once to set variable value in init

--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -2,63 +2,64 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	private double[] ratio;
-	private int trials, numSites;
-    private double CONF_95 = 1.96;
-    private double sd;
+    private double[] ratio;
+    private int trials, numSites;
+    private double conf = 1.96; // 95 % confidence level
+    private double sd, av, sqrtTrials;
 
-	public PercolationStats(int n, int trials) {
-		if (n <= 0 || trials <= 0) {
-			throw new IllegalArgumentException();
-		}
+    public PercolationStats(int n, int trls) {
+        if (n <= 0 || trls <= 0) {
+            throw new IllegalArgumentException();
+        }
 
-		this.trials = trials;
-		ratio = new double[trials];
-		numSites = n*n;
+        trials = trls;
+        sqrtTrials = Math.sqrt(trials);
+        ratio = new double[trials];
+        numSites = n*n;
 
-		// perform trials on n x n grid
-		for (int t = 0; t < trials; ++t) {
-			Percolation p = new Percolation(n);
-			int[] perm = StdRandom.permutation(numSites);
-			int ptr = 0;
-			while (!p.percolates()) {
-				int index = perm[ptr];
-				int row = index/n + 1;
-				int col = index % n + 1;
-				// open new site
-				p.open(row, col);
-				++ptr;
-			}
-			ratio[t] = p.numberOfOpenSites() * 1.0 / numSites;
-		}
-        sd = stddev();
-	}
+        // perform trials on n x n grid
+        for (int t = 0; t < trials; t++) {
+            Percolation p = new Percolation(n);
+            int[] perm = StdRandom.permutation(numSites);
+            int i = 0; // counting index for permutation
+            while (!p.percolates()) {
+                int index = perm[i++];
+                int row = index/n + 1;
+                int col = index % n + 1;
+                // open new site
+                p.open(row, col);
+            }
+            ratio[t] = p.numberOfOpenSites() * 1.0 / numSites;
+        }
+        sd = StdStats.stddev(ratio);
+        av = StdStats.mean(ratio);
+    }
 
-	public double mean() {
-		// sample mean of percolation threshold
-		return StdStats.mean(ratio);
-	}
+    public double mean() {
+        // sample mean of percolation threshold
+        return av;
+    }
 
-	public double stddev() {
-		return StdStats.stddev(ratio);
-	}
+    public double stddev() {
+        return sd;
+    }
 
-	public double confidenceLo() {
-		// lower bound of 95% confidence interval
-		return mean() - CONF_95*sd / Math.sqrt(trials);
-	}
+    public double confidenceLo() {
+        // lower bound of 95% confidence interval
+        return av - conf * sd / sqrtTrials;
+    }
 
-	public double confidenceHi() {
-		// upper bound of 95% confidence interval
-		return mean() + CONF_95*sd / Math.sqrt(trials);
-	}
+    public double confidenceHi() {
+        // upper bound of 95% confidence interval
+        return av + conf * sd / sqrtTrials;
+    }
 
-	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
-		int testCase = Integer.parseInt(args[1]);
-		PercolationStats tester = new PercolationStats(n, testCase);
-		System.out.format("mean                    = %f\n", tester.mean());
-		System.out.format("stddev                  = %f\n", tester.stddev());
-		System.out.format("95%% confidence interval = %f to %f\n", tester.confidenceLo(), tester.confidenceHi());
-	}
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int testCase = Integer.parseInt(args[1]);
+        PercolationStats tester = new PercolationStats(n, testCase);
+        System.out.format("mean                    = %f\n", tester.mean());
+        System.out.format("stddev                  = %f\n", tester.stddev());
+        System.out.format("95%% confidence interval = %f to %f\n", tester.confidenceLo(), tester.confidenceHi());
+    }
 }
