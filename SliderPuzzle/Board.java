2c1e115c686b8d37169a2fa818568bcf79647f61
Cameron McCormack <cam.mac@live.co.uk>
2019-09-03 21:32:09 +0100

    first commit 76/100

---
+++ SliderPuzzle/Board.java
@@ -0,0 +1,170 @@
+import java.util.LinkedList;
+
+public class Board {
+    private int n;
+    private int[][] board;
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+        n = tiles.length;
+
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = tiles[i][j];
+            }
+        }
+        board = copy;
+    }
+
+    // string representation of this board
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append(n + "\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                str.append(board[i][j] + " ");
+            }
+        }
+        str.append("\n");
+        return str.toString();
+    }
+
+    // board dimension n
+    public int dimension() {
+        return n;
+    }
+
+    // number of tiles out of place
+    public int hamming() {
+        int wrong = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!tileInPlace(i, j)) {
+                    wrong++;
+                }
+            }
+        }
+        return wrong;
+    }
+
+    // check if a tile is in place
+    private boolean tileInPlace(int row, int col) {
+        int tile = board[row][col];
+        return (tile == 0 || tile == (row * n + col + 1));
+    }
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += manhattanDistance(i, j);
+            }
+        }
+        return sum;
+    }
+
+    // calculate the minimum number of moves to put a piece in place
+    private int manhattanDistance(int row, int col) {
+        int tile = board[row][col];
+        if (tile == 0) {
+            return 0;
+        } else {
+            int target_row = (tile - 1) / n;
+            int target_col = (tile - 1) % n;
+            return Math.abs(row - target_row) + Math.abs(col - target_col);
+        }
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!tileInPlace(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+        if (y == this) {
+            return true;
+        }
+        if (y == null || !(y instanceof Board)) {
+            return false;
+        }
+        if (y.toString() == this.toString()) {
+            return true;
+        }
+        return false;
+    }
+
+    private int[][] swap(int row1, int col1, int row2, int col2) {
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = board[i][j];
+            }
+        }
+        int temp = copy[row1][col1];
+        copy[row1][col1] = copy[row2][col2];
+        copy[row2][col2] = temp;
+        return copy;
+    }
+
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        LinkedList<Board> neighbors = new LinkedList<Board>();
+
+        int spaceRow = 0;
+        int spaceCol = 0;
+
+        // find space
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    spaceRow = i;
+                    spaceCol = j;
+                }
+            }
+        }
+
+        if (spaceRow > 0) {
+            neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow+1, spaceCol)));
+        }
+        if (spaceRow < n - 1) { 
+            neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow-1, spaceCol)));
+        }
+        if (spaceCol > 0) {
+            neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow, spaceCol+1)));
+        }
+        if (spaceCol < n - 1) {
+            neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow, spaceCol-1)));
+        }
+
+        return neighbors;
+    }
+
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((board[i][j] != 0) && (board[i][j+1] != 0)) {
+                    return new Board(swap(i, j, i, j+1));
+                }
+            }
+        }
+        throw new RuntimeException();
+    }
+
+    // quick test
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ SliderPuzzle/Solver.java
@@ -0,0 +1,163 @@
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class Solver {
+    private List<Board> solutionBoards = new ArrayList<>();
+    private boolean solved;
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        MinPQ<SolverStep> steps = new MinPQ<>(new SolverStepComparator());
+        steps.insert(new SolverStep(initial, 0, null));
+
+        MinPQ<SolverStep> stepsTwin = new MinPQ<>(new SolverStepComparator());
+        stepsTwin.insert(new SolverStep(initial.twin(), 0, null));
+
+        SolverStep step, stepTwin;
+        while (!steps.min().getBoard().isGoal() && !stepsTwin.min().getBoard().isGoal()) {
+            step = steps.delMin();
+            for (Board neighbor : step.getBoard().neighbors()) {
+                if (!inPath(step, neighbor)) {
+                    steps.insert(new SolverStep(neighbor, step.getMoves() + 1, step));
+                }
+            }
+
+            stepTwin = stepsTwin.delMin();
+            for (Board neighbor : stepTwin.getBoard().neighbors()) {
+                if (!inPath(stepTwin, neighbor)) {
+                    stepsTwin.insert(new SolverStep(neighbor, stepTwin.getMoves() + 1, stepTwin));
+                }
+            }
+        }
+        
+        step = steps.delMin();
+        solved = step.getBoard().isGoal();
+
+        solutionBoards.add(step.getBoard());
+        while ((step = step.getPreviousStep()) != null) {
+            solutionBoards.add(0, step.getBoard());
+        }
+    }
+
+    private static class SolverStep {
+        private int moves;
+        private Board board;
+        private SolverStep previousStep;
+
+        private SolverStep(Board brd, int mov, SolverStep prev) {
+            moves = mov;
+            board = brd;
+            previousStep = prev;
+        }
+
+        public int getMoves() {
+            return moves;
+        }
+
+        public int getPriority() {
+            return board.manhattan() + moves;
+        }
+
+        public Board getBoard() {
+            return board;
+        }
+
+        public SolverStep getPreviousStep() {
+            return previousStep;
+        }
+    }
+
+    private class SolutionIterator implements Iterator<Board> {
+        private int index = 0;
+
+        public boolean hasNext() {
+            return index < solutionBoards.size();
+        }
+
+        public Board next() {
+            return solutionBoards.get(index++);
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private boolean inPath(SolverStep step, Board board) {
+        SolverStep previousStep = step;
+        while ((previousStep = previousStep.getPreviousStep()) != null) {
+            if (previousStep.getBoard().equals(board)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // is the initial board solvable?
+    public boolean isSolvable() {
+        return solved;
+    }
+
+    // min number of moves to solve initial board
+    public int moves() {
+        if (isSolvable()) {
+            return solutionBoards.size() - 1;
+        } else {
+            return -1;
+        }
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+        Iterable<Board> iterable;
+        if (isSolvable()) {
+            iterable = new Iterable<Board>() {
+                @Override
+                public Iterator<Board> iterator() {
+                    return new SolutionIterator();
+                }
+            };
+        } else {
+            iterable = null;
+        }
+        return iterable;
+    }
+
+    private static class SolverStepComparator implements Comparator<SolverStep> {
+        public int compare(SolverStep step1, SolverStep step2) {
+            return step1.getPriority() - step2.getPriority();
+        }
+    }
+
+    // test client
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tiles[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable()) {
+            StdOut.println("No solution possible.");
+        } else {
+            StdOut.print("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution()) {
+                StdOut.println(board);
+            }
+        }
+    }
+}
