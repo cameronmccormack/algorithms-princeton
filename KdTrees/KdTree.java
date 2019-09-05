b28c125f8d49f8900fccedc49ce7b97f17a19039
Cameron McCormack <cam.mac@live.co.uk>
2019-09-05 20:51:22 +0100

    skeleton api for kdtree

---
+++ KdTrees/KdTree.java
@@ -0,0 +1,52 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import java.util.LinkedList;
+import java.util.List;
+
+public class KdTree {
+
+    // construct an empty set of points
+    public KdTree() {
+
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+
+    }
+
+    // number of points in the set
+    public int size() {
+
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+
+    }
+
+    // a nearest neightbor in the set to point p
+    public Point2D nearest(Point2D p) {
+
+    }
+
+    // quick test
+    public static void main(String[] args) {
+
+    }
+}
