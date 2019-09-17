480d60726cbe416a61fefe71c723d812708c2abe
Cameron McCormack <cam.mac@live.co.uk>
2019-09-17 17:58:58 +0100

    first draft of CSA with test complete

--- BurrowsWheeler/CircularSuffixArray.java
+++ BurrowsWheeler/CircularSuffixArray.java
@@ -1,8 +1,10 @@
 import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdOut;
 
 public class CircularSuffixArray {
     private int length;
     private int[] index;
+    private ST<String, Integer> circulations;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
@@ -10,7 +12,7 @@ public class CircularSuffixArray {
             throw new IllegalArgumentException();
         }
         length = s.length();
-        ST<String, Integer> circulations = new ST<String, Integer>();
+        circulations = new ST<String, Integer>();
         circulations.put(s, 0);
         for (int i = 1; i < length; i++) {
             circulations.put(s.substring(i, length) + s.substring(0, i), i);
@@ -24,16 +26,26 @@ public class CircularSuffixArray {
 
     // length of s
     public int length() {
-        return 1;
+        return length;
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        return 1;
+        if (i < 0 || i >= length) {
+            throw new IndexOutOfBoundsException();
+        }
+        return index[i];
     }
 
     // unit test
     public static void main(String[] args) {
-
+        CircularSuffixArray csa = new CircularSuffixArray("BADCFGE");
+        StdOut.println("Input string is BADCFGE.");
+        StdOut.printf("Length is %d (should be 7).\n", csa.length());
+        StdOut.print("Index positions of CSA are: ");
+        for (int i = 0; i < csa.length(); i++) {
+            StdOut.print(csa.index(i));
+        }
+        StdOut.println(" (should be 1032645).");
     }
 }
