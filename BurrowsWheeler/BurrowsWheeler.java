cc0ff55f1300f579d49b772ddbb10456b5adf1d3
Cameron McCormack <cam.mac@live.co.uk>
2019-09-18 16:26:23 +0100

    rewrote array structure for inverse transform function

--- BurrowsWheeler/BurrowsWheeler.java
+++ BurrowsWheeler/BurrowsWheeler.java
@@ -4,7 +4,8 @@ import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.Queue;
 
 public class BurrowsWheeler {
-    private static final int BITS = 8; // ASCII
+    private static final int R = 256; // ASCII
+    private static final int BITS = 8; // 8 bit output
 
     // apply Burrows-Wheeler transform
     // reading from standard input and writing to standard output
@@ -35,27 +36,38 @@ public class BurrowsWheeler {
         }
     }
 
-    // apply Burrow-Wheeler inverse transform
+    // apply Burrows-Wheeler inverse transform
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
-        int first = BinaryStdIn.readInt();
+        // read first position and array of characters
+        int firstPosition = BinaryStdIn.readInt();
         String str = BinaryStdIn.readString();
         char[] chars = str.toCharArray();
+
+        // array to store the next array or chars
         int[] next = new int[chars.length];
-        ST<Character, Queue<Integer>> positions = new ST<Character, Queue<Integer>>();
+        // array to store the 1st character of the sorted suffixes
+        char[] first = new char[chars.length];
+        // array to store the total count for each character
+        int[] count = new int[R+1];
+
+        // key-index counting, storing values in next array
         for (int i = 0; i < chars.length; i++) {
-            if (!positions.contains(chars[i])) {
-                positions.put(chars[i], new Queue<Integer>());
-            }
-            positions.get(chars[i]).enqueue(i);
+            count[chars[i]+1]++;
+        }
+        for (int i = 0; i < R; i++) {
+            count[i+1] += count[i];
         }
         for (int i = 0; i < chars.length; i++) {
-            next[i] = positions.get(chars[i]).dequeue();
+            next[count[chars[i]]] = i;
+            first[count[chars[i]]++] = chars[i];
         }
-        int position = first;
+
+        // write out to standard output
+        int j = firstPosition;
         for (int i = 0; i < chars.length; i++) {
-            BinaryStdOut.write(chars[position]);
-            position = next[position];
+            BinaryStdOut.write(first[j]);
+            j = next[j];
         }
         BinaryStdOut.close();
     }
